851676c5dfd828aabe489bd35a5c71b195456226
Eric N. Gomes <eric.n.gomes@hotmail.com>
2019-03-04 10:47:16 +0000

    Bulma Page
    Having Fun with Bulma

---
+++ Ferrari Landing Page/styles/style.js
@@ -0,0 +1,13 @@
+
+/*
+var url = ['images/ferrari-1.jpg',
+           'images/ferrari-2.jpg'];
+
+           currentImageIndex = 0;
+           setInterval(function(){
+             console.log(url[currentImageIndex])
+             var p = $('.hero');
+             p.css("background","url("+url[currentImageIndex++] + ")");
+             if( currentImageIndex >= url.length){currentImageIndex = 0}
+           }, 1000);
+*/
